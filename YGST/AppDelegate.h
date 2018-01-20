e3450205c753f27f61020447272acb47382bfdad
yalong zhang <yalongzhang@yalongdeimac.local>
2018-01-20 13:44:19 +0800

    Initial Commit

---
+++ YGST/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YGST
+//
+//  Created by yalong zhang on 2018/1/20.
+//  Copyright © 2018年 yalong zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YGST/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YGST
+//
+//  Created by yalong zhang on 2018/1/20.
+//  Copyright © 2018年 yalong zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
